f89282fb4f6a47774d8ae5dc73c7e24db6a0fc57
yhj0904 <ujk6073@gmail.com>
2026-03-09 18:29:59 +0900

    :tada: first commit !!!

---
+++ src/main/java/com/monglepick/monglepickbackend/MonglepickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monglepick.monglepickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonglepickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonglepickBackendApplication.class, args);
+    }
+
+}
